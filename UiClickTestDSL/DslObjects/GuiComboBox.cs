93865f91d31699f5662448cb34aebfa399b8ad62
Bendik August Nesbø <gulost@gmail.com>
2015-07-21 12:26:36 +0200

    Added a new way of selecting an item from a GuiComboBox, SelectItemContaining.

--- UiClickTestDSL/DslObjects/GuiComboBox.cs
+++ UiClickTestDSL/DslObjects/GuiComboBox.cs
@@ -67,6 +67,14 @@ namespace UiClickTestDSL.DslObjects {
             item.First().Select();
         }
 
+        public void SelectItemContaining(string caption) {
+            List<GuiComboBoxItem> all = GetAllItems();
+            IEnumerable<GuiComboBoxItem> item = from i in all
+                                                where i.Text.Contains(caption)
+                                                select i;
+            item.First().Select();
+        }
+
         private static bool RegexMatch(string text, string caption) {
             return text == caption || Regex.IsMatch(text, @"(.* name\:|\[.*,) " + caption + @"(\]){0,1}") || text.Trim().EndsWith(": " + caption);
         }
