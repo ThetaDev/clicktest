3ca51d7199a53a30d1b49eae8dd1c8fab36c5b5c
Bendik August Nesbø <bendik.nesbo@gmail.com>
2017-01-13 12:13:32 +0100

    Added method SelectRowByCellContent (a combination of FindRowByCellContent and SelectRow)

--- UiClickTestDSL/DslObjects/GuiDataGrid.cs
+++ UiClickTestDSL/DslObjects/GuiDataGrid.cs
@@ -114,6 +114,21 @@ namespace UiClickTestDSL.DslObjects {
             throw new Exception(string.Format("Error: No row with {0} in column {1} found!", content, columnName));
         }
 
+        public void SelectRowByCellContent(string columnName, string content) {
+            int foundRow = -1;
+            int colIndex = HeaderNamesToIndex[columnName];
+            for (int i = 0; i < RowCount; i++) {
+                var cell = Cell(i, colIndex);
+                if (cell.Text == content) {
+                    foundRow = i;
+                    break;
+                }
+            }
+            if (foundRow == -1)
+                throw new Exception(string.Format("Error: No row with {0} in column {1} found!", content, columnName));
+            SelectRow(foundRow);
+        }
+
         private SelectionItemPattern GetRowSelectionPattern(int rowIndex) {
             var rows = dgAutoEl.FindAllChildrenByClassName("DataGridRow");
             Console.WriteLine("found rows: " + rows.Count() + ";  index to select: " + rowIndex);
