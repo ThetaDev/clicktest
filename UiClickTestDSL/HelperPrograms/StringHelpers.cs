1a92e7fb7b5a7274fc35659ff4b41286041fb480
gkopheim <gkopheim@gmail.com>
2018-07-11 14:37:31 +0200

    Added helper method for counting substrings in a string

--- UiClickTestDSL/HelperPrograms/StringHelpers.cs
+++ UiClickTestDSL/HelperPrograms/StringHelpers.cs
@@ -10,5 +10,9 @@ namespace UiClickTestDSL.HelperPrograms {
             //string.Contains has no overload for ignoring case, so using IndexOf instead.
             return text.IndexOf(substring, StringComparison.CurrentCultureIgnoreCase) >= 0;
         }
+        public static int SubstringCount(this string haystack, string needle) {
+            //Counts occurrances of "needle" in "haystack".
+            return haystack.Split(new[] { needle }, StringSplitOptions.None).Length - 1;
+        }
     }
 }
